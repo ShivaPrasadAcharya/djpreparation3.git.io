9069bfd05fb14d35a03afcdc07fd07ed68985978
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2026-02-17 17:19:11 +0545

    Update dataYmarkdown0.js

--- dataYmarkdown0.js
+++ dataYmarkdown0.js
@@ -1,1 +1,9 @@
-
+// dataYmarkdown0.js
+window.markdown0Content = `
+# Markdown content
+Your markdown content goes here.
+`;
+window.markdown0Meta = { 
+  name: " sample-copyable",
+   emoji: "📄"
+   };
